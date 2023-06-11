03199ad84baafb5868c426ca472d4880f0116673
Saad Hesham <132556058+Saad-Hisham@users.noreply.github.com>
2023-06-11 11:39:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-// create a map object with center and zoom level
-var map = L.map('mapid').setView([51.505, 167.2], 13);
+// call the elements 
+var map = L.map('mapid').setView([51.505, 167.2], 13); 
 const locations = document.getElementById("location");
-// Get the time element
 const times = document.getElementById("time");
-// Get the ISP element
 const isps = document.getElementById("isp");
-
 const inputElement = document.getElementById("ip-input");
 const validationMessageElement = document.getElementById("validation-message");
 const submitButton = document.getElementById("submit-button");
 const ip = document.getElementById("ip");
+// this reg ex to validate the input 
 const ipRegex = /^([0-9]{1,3}\.){3}[0-9]{1,3}$/;
+// ip address
 var ipAddress = "192.212.174.101";
-
 var ipifyApiKey = "https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_szq6m2u0VFInEaYnXApI2qXSVZ0Gl&ipAddress=8.8.8.8";
 
-// replace IP_ADDRESS with the IP address you want to geolocate
-
-// construct the URL for the API request
 var apiUrl = 'https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_szq6m2u0VFInEaYnXApI2qXSVZ0Gl&ipAddress=8.8.8.8' + ipifyApiKey + '&ipAddress=' + ipAddress;
 
 // send a request to the API
@@ -36,23 +31,19 @@ fetch(apiUrl)
       .bindPopup('This is the IP location: ' + data.location.city + ', ' + data.location.timezone)
       .openPopup();
 
-    // recenter the map to the IP location
     map.setView([lat, lon], 13);
   })
   .catch(error => console.error(error));
 submitButton.addEventListener("click", () => {
   const inputValue = inputElement.value;
   if (ipRegex.test(inputValue)) {
-    // The input value is a valid IP address
     validationMessageElement.style.display = "none";
     inputElement.style.border = "2px solid green";
 
     var ipifyApiKey = "https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_szq6m2u0VFInEaYnXApI2qXSVZ0Gl&ipAddress=8.8.8.8";
 
-    // replace IP_ADDRESS with the IP address you want to geolocate
     var ipAddress = inputElement.value;
 
-    // construct the URL for the API request
     var apiUrl = 'https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_szq6m2u0VFInEaYnXApI2qXSVZ0Gl&ipAddress=8.8.8.8' + ipifyApiKey + '&ipAddress=' + ipAddress;
 
     // send a request to the API
@@ -67,32 +58,25 @@ submitButton.addEventListener("click", () => {
         isps.innerHTML = data.isp
         ip.innerHTML = inputElement.value
         console.log(data)
-        // create a marker for the IP location and add it to the map
         L.marker([lat, lon]).addTo(map)
           .bindPopup('This is the IP location: ' + data.location.city + ', ' + data.location.timezone)
           .openPopup();
 
-        // recenter the map to the IP location
         map.setView([lat, lon], 13);
       })
       .catch(error => console.error(error));
 
-    // Do something with the valid IP address, such as send it to a server
   } else {
-    // The input value is not a valid IP address
     validationMessageElement.style.display = "inline";
     inputElement.style.border = "2px solid red";
   }
 });
 
-// add a tile layer to the map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
   maxZoom: 18
 }).addTo(map);
 
-// add a marker to the map
 L.marker([51.5, -0.09]).addTo(map)
   .bindPopup('search ip')
   .openPopup();
-// replace YOUR_API_KEY with your actual API key
